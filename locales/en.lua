a9db3dde374336e58d84eef6446636c7c952ea66
Mobius1 <mobius1@gmx.com>
2020-08-15 15:27:26 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -3,8 +3,8 @@ Locales["en"] = {
     ['cmd_current_lvl'] = "Your current rank is ^*^2 %s",
     ['cmd_next_lvl']    = "You require ^*^2 %s XP ^*^7to advance to rank ^*^2 %s",
 
-    ['err_xp_update']   = "esx_xp: Invalid rank (%s) passed to '%s'",
-    ['err_lvl_update']  = "esx_xp: Invalid rank (%s) passed to '%s'",
-    ['err_lvls_check']  = "esx_xp: You have an error in %s ranks in '%s'",
+    ['err_xp_update']   = "XpM: Invalid rank (%s) passed to '%s'",
+    ['err_lvl_update']  = "XpM: Invalid rank (%s) passed to '%s'",
+    ['err_lvls_check']  = "XpM: You have an error in %s ranks in '%s'",
     ['err_lvl_check']   = "Rank %s: %s",
 }
